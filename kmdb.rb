0da9e1883187db3c285e7d6fce1c878085fe2842
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-05-03 22:34:04 +0000

    actor info insert

--- kmdb.rb
+++ kmdb.rb
@@ -118,9 +118,54 @@ new_movie["rated"] = "PG-13"
 new_movie["studio_id"] = warner_bros["id"]
 new_movie.save
 
+# puts Movie.all.inspect
 
-puts Movie.all.inspect
+# insert new rows in actors table
+new_actor = Actor.new
+new_actor["name"] = "Christian Bale"
+new_actor.save
 
+new_actor = Actor.new
+new_actor["name"] = "Michael Caine"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Liam Neeson"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Katie Holmes"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Gary Oldman"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Heath Ledger"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Aaron Eckhart"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Maggie Gyllenhaal"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Tom Hardy"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Joseph Gordon-Levitt"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Anne Hathaway"
+new_actor.save
+
+# puts Actor.all.inspect
 
 # Prints a header for the movies output
 puts "Movies"
